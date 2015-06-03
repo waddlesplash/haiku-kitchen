a2b120f2c51309823aadbdea59edb0cf58409321
Augustin Cavalier <waddlesplash@gmail.com>
2015-06-03 19:56:18 -0400

    Caching cleanup, run git-pull every 10 minutes.

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@
  *		Augustin Cavalier <waddlesplash>
  */
 
-var log = require('debug')('kitchen:index'), fs = require('fs'),
-	shell = require('shelljs'), glob = require('glob'),
-	Recipe = require('./recipe.js').recipe;
+var log = require('debug')('kitchen'), fs = require('fs'),
+	shell = require('shelljs'), timers = require('timers'),
+	glob = require('glob'), Recipe = require('./recipe.js').recipe;
 
 if (!shell.which('git')) {
 	log('FATAL: git must be installed to run this script.');
@@ -36,27 +36,34 @@ log("starting up");
 
 /*! --------------------- haikuports tree --------------------- */
 var fRecipes, fClientRecipes;
-function createCache() {
-	log('cache: creating recipe metadata cache...');
-	fRecipes = [];
+function updateClientCache() {
 	fClientRecipes = [];
-	var files = glob.sync('cache/haikuports/*-*/*/*.recipe');
-	for (var i in files) {
-		var recipe = new Recipe(files[i]);
-		fRecipes.push(recipe);
+	for (var i in fRecipes) {
 		fClientRecipes.push({
-			name: recipe.name,
-			category: recipe.category,
-			version: recipe.version,
-			revision: recipe.revision,
+			name: fRecipes[i].name,
+			category: fRecipes[i].category,
+			version: fRecipes[i].version,
+			revision: fRecipes[i].revision,
 			lint: '?'
 		});
 	}
 	fClientRecipes = JSON.stringify(fClientRecipes);
-	log('cache: recipe metadata cache build complete.');
 }
-
-if (!fs.existsSync('cache/haikuports/')) {
+function updateCacheFor(files) {
+	log('cache: updating ' + files.length + ' entries...');
+	for (var i in files) {
+		var recipe = new Recipe(files[i]);
+		fRecipes[recipe.name + '-' + recipe.version] = recipe;
+	}
+	updateClientCache();
+	log('cache: recipe metadata update complete.');
+}
+function completeCacheRebuild() {
+	fRecipes = {};
+	updateCacheFor(glob.sync('cache/haikuports/*-*/*/*.recipe'));
+}
+function createCache() {
+	log('cache: creating cache from scratch...');
 	shell.rm('-rf', 'cache');
 	shell.mkdir('cache');
 	shell.cd('cache');
@@ -68,8 +75,33 @@ if (!fs.existsSync('cache/haikuports/')) {
 			process.exit(3);
 		}
 	shell.cd('..');
+	completeCacheRebuild();
 }
-createCache();
+
+if (!fs.existsSync('cache/haikuports/')) {
+	createCache();
+} else {
+	completeCacheRebuild();
+}
+
+function updateHaikuportsTree() {
+	log('running git-pull...');
+	shell.cd('cache/haikuports');
+		shell.exec('git pull --ff-only', {silent: true}, function (code, output) {
+			if (code) {
+				log('git-pull failed, deleting cache');
+				createCache();
+			} else if (output.indexOf('Already up-to-date.') >= 0) {
+				log('git-pull finished, no changes');
+			} else {
+				log('git-pull finished, updating cache...');
+				completeCacheRebuild();
+			}
+		});
+	shell.cd('../..');
+}
+updateHaikuportsTree();
+timers.setInterval(updateHaikuportsTree, 10 * 60 * 1000);
 
 /*! ------------------------ webserver ------------------------ */
 var express = require('express'), app = express();
