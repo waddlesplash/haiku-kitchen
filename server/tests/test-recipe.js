6850babd65dcd973f753279e577489701c649d9f
Augustin Cavalier <waddlesplash@gmail.com>
2015-07-17 17:09:51 -0400

    Initial unit tests.

---
+++ server/tests/test-recipe.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2015 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Augustin Cavalier <waddlesplash>
+ */
+
+var Recipe = require('../recipe.js');
+
+module.exports = {
+    'generic-1.0.1': function (test) {
+		var generic = new Recipe('recipe/generic-1.0.1.recipe');
+        test.strictEqual(generic.name, 'generic');
+		test.strictEqual(generic.version, '1.0.1');
+		test.strictEqual(generic.revision, '1');
+		test.strictEqual(JSON.stringify(generic.provides),
+			JSON.stringify(['projectx = $portVersion', 'cmd:projectx = $portVersion']));
+		test.strictEqual(JSON.stringify(generic.requires),
+			JSON.stringify(['haiku']));
+		test.strictEqual(JSON.stringify(generic.build_requires),
+			JSON.stringify(['haiku_devel', 'cmd:make', 'cmd:gcc']));
+		test.strictEqual(JSON.stringify(generic.architectures),
+			JSON.stringify(['x86_gcc2', '?x86', '?x86_64']));
+        test.done();
+    }
+};
---
+++ server/tests/test.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2015 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Augustin Cavalier <waddlesplash>
+ */
+
+var reporter = require('nodeunit').reporters.default;
+reporter.run(['.']);
