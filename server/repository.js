51a594b02065736d066ab95dba4f664c7f8bac5d
Augustin Cavalier <waddlesplash@gmail.com>
2015-07-31 16:16:39 -0400

    server/repository: Add recipe-list preprocessor.
    Does essentially everything but dependency resolving.

--- server/repository.js
+++ server/repository.js
@@ -8,8 +8,11 @@
 
 var log = require('debug')('kitchen:repository');
 
-var primaryArches = ['x86_gcc2', 'x86', 'x86_64'],
-	secondaryArches = ['x86_gcc2:x86', 'x86:x86_gcc2'];
+var arches = [
+	['x86_gcc2', 'x86'],
+	['x86', 'x86_gcc2'],
+	['x86_64']
+];
 
 /**
   * @class RepositoryManager
@@ -23,7 +26,9 @@ var primaryArches = ['x86_gcc2', 'x86', 'x86_64'],
   */
 module.exports = function (builderManager, buildsManager) {
 	/**
-	  * Creates a job to build the specified recipes.
+	  * @public
+	  * @memberof! RepositoryManager.prototype
+	  * @description Creates a job to build the specified recipes.
 	  * @param {array} recipes The recipes to build.
 	  * @param {string} desc The description of the build.
 	  */
@@ -40,8 +45,109 @@ module.exports = function (builderManager, buildsManager) {
 			}
 		};
 		for (var i in recipes) {
-			build.steps.push({command: 'haikuporter --get-dependencies ' + recipes[i]});
+			build.steps.push({command: 'haikuporter --no-dependencies ' + recipes[i]});
 		}
 		buildsManager.addBuild(build);
+	};
+
+	/**
+	  * @public
+	  * @memberof! RepositoryManager.prototype
+	  * @description Determines what ports need to be built.
+	  * @param {string} arch The primary architecture to build for.
+	  * @returns {array} recipes An ordered list of recipes to build.
+	  */
+	this.determinePortsToBuild = function (arch) {
+		function versionGreaterThan (v1, v2) {
+			if (v1 === undefined && v2 !== undefined)
+				return true;
+			if (v2 === undefined && v1 !== undefined ||
+				v1 === undefined && v2 === undefined)
+				return false;
+			var v1a = v1.split('.'), v2a = v2.split('.');
+			for (var i = 0; i < Math.min(v1.length, v2.length); i++) {
+				// First see if they aren't numbers.
+				var is1int = /^\d+$/.test(v1a[i]), is2int = /^\d+$/.test(v2a[i]);
+				if (!is1int && !is2int) {
+					if (v1a[i] != v2a[i])
+						return v2a[i] > v1a[i];
+					continue;
+				}
+				if (!is1int && is2int)
+					return true;
+				if (is1int && !is2int)
+					return false;
+
+				// Now interpret them as numbers.
+				var v1iI = parseInt(v1a[i]), v2iI = parseInt(v2a[i]);
+				if (v2iI > v1iI)
+					return true;
+				if (v2iI < v1iI)
+					return false;
+			}
+			if (v2a.length > v1a.length)
+				return true;
+			return false;
+		}
+
+		var secondaryArch = undefined;
+		for (var i in arches) {
+			if (arches[i][0] == arch)
+				secondaryArch = arches[i][1];
+		}
+
+		// Pass 1: Find the highest-version recipe for the arch.
+		var highestVersionForArch = {};
+		for (var i in global.portsTree.recipes) {
+			var recipe = global.portsTree.recipes[i];
+			if (recipe.architectures.indexOf(arch) == -1)
+				continue;
+			if (highestVersionForArch[recipe.name] === undefined ||
+				versionGreaterThan(highestVersionForArch[recipe.name].version, recipe.version)) {
+				highestVersionForArch[recipe.name] = recipe;
+			}
+		}
+
+		// Pass 2: Replace $secondaryArchSuffix, strip versions from PROVIDES/REQUIRES in prep.
+		// for passing stuff to the depsolver.
+		var processedRecipes = {};
+		function processItem(str, recipe) {
+			str = str
+				.replace(/\${secondaryArchSuffix}/g, '')
+				.replace(/\$secondaryArchSuffix\b/g, '')
+				.replace(/\$portVersion\b/g, recipe.version)
+				.replace(/\${portVersion}/g, recipe.version)
+				.replace(/\$portName\b/g, recipe.name)
+				.replace(/\${portName}/g, recipe.name);
+			var ioS = str.indexOf(' ');
+			if (ioS != -1)
+				str = str.substr(0, ioS);
+			if (str == 'haiku' || str == 'haiku_devel')
+				return '';
+			return str;
+		}
+		for (var i in highestVersionForArch) {
+			var recipe = highestVersionForArch[i];
+			var provides = [], requires = [];
+			for (var i in recipe.provides) {
+				var procd = processItem(recipe.provides[i], recipe);
+				if (procd.length == 0)
+					continue;
+				provides.push(procd);
+			}
+			for (var i in recipe.build_requires) {
+				var procd = processItem(recipe.build_requires[i], recipe);
+				if (procd.length == 0)
+					continue;
+				requires.push(procd);
+			}
+			processedRecipes[recipe.name] = {
+				name: recipe.name,
+				version: recipe.version,
+				provides: provides,
+				requires: requires
+			};
+		}
+		console.log(processedRecipes);
 	};
 };
